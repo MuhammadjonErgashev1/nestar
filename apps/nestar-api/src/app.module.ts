24fac0de022da220d94f1c1deb692b08da3ab6dc
MuhammadjonErgashev1 <mmuhammadjonergashev1@gmail.com>
2025-12-31 10:12:09 +0900

    feat: develop global GraphQL error handling system

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -7,6 +7,9 @@ import { ApolloDriver  } from '@nestjs/apollo'
 import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
+import { error } from 'console';
+import { T } from './libs/types/common';
+
 
 @Module({
   imports: [
@@ -15,7 +18,18 @@ import { DatabaseModule } from './database/database.module';
       playground: true,
       uploads: false,
       autoSchemaFile: true,
-    }), ComponentsModule, DatabaseModule
+      formatError: (error: T) =>{
+        const graphQLFormattedError = {
+          code: error?.extensions.code,
+          message:
+          error?.extensions?.exception?.response?.message || error?.extensions?.response?.message || error?.message,
+        }
+        console.log('GRAPHQL GLOBAL ERR:', graphQLFormattedError)
+        return graphQLFormattedError;
+      }
+    }),
+     ComponentsModule, 
+     DatabaseModule
   ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
