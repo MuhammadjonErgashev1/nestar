7fb74790ab018db87f872e6eac70f44ba87b4c92
MuhammadjonErgashev1 <mmuhammadjonergashev1@gmail.com>
2026-01-12 13:40:51 +0900

    feat: develop getProperties GraphQL api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -1,7 +1,7 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PropertyService } from './property.service';
-import { Property } from '../../libs/dto/property/property';
-import { PropertyInput } from '../../libs/dto/property/property.input';
+import { Properties, Property } from '../../libs/dto/property/property';
+import { PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import {  UseGuards } from '@nestjs/common';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
@@ -52,5 +52,14 @@ export class PropertyResolver {
         return await this.propertyService.updateProperty(memberId,input)
     }
 
+    @UseGuards(WithoutGuard)
+    @Query(() => Properties)
+     public async getProperties(
+    @Args('input') input: PropertiesInquiry,
+    @AuthMember('_id') memberId: ObjectId,
+    ):Promise<Properties>{
+        console.log('Query: getProperties');
+       return await this.propertyService.getProperties(memberId,input)
+    }
 
 }
