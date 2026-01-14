a0c462c568c2d83ae8f46b76344deb5f8868f9ab
MuhammadjonErgashev1 <mmuhammadjonergashev1@gmail.com>
2026-01-14 14:34:59 +0900

    feat: develop removeCommentByAdmin GraphQL api

--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -117,4 +117,10 @@ public async getComments(memberId: ObjectId, input: CommentsInquiry): Promise<Co
 
     return result[0];
 }
+
+public async removeCommentByAdmin(input: ObjectId): Promise<Comment> {
+    const result = await this.commentModel.findByIdAndDelete(input).exec();
+    if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+    return result;
+}
 }
